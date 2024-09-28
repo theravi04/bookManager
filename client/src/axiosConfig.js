75f0408c3623d684289096bb80423f1309a2c10f
Ravi Kumar <04ravi.rk@gmail.com>
2024-09-29 00:05:07 +0530

    changes in client

--- client/src/axiosConfig.js
+++ client/src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:4000/api',
+    baseURL: 'https://bookmanager-tuo3.onrender.com/api',
     headers: {
         'Content-Type': 'application/json'
     }
